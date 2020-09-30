36fa2ff3c91745166c38aa4e253e724ab4541618
Ali Sulieman <ahsuliem@uno.edu>
2020-09-29 20:58:52 -0500

    added two more functions and mapped it with url

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,5 +2,13 @@ from django.http import HttpResponse
 
 
 
-def home_page(request)
+def home_page(request):
     return HttpResponse("<h1> hello world <h1>")
+
+
+def about_page(request):
+    return HttpResponse("<h1> about us <h1>")
+
+
+def contact_page(request):
+    return HttpResponse("<h1> contach us <h1>")
